a21c8d086ed7a9b28c98d546848e39d04a4f1669
sereja-s <serejasuvorov1979@gmail.com>
2022-01-02 19:58:37 +0300

    Добавлены картинки

--- shop_logo/js/script.js
+++ shop_logo/js/script.js
@@ -3986,35 +3986,37 @@ if (quantityButtons.length > 0) {
 		});
 	}
 }
-// Настройки для слайдера фильтра цены: 
-const priceSlider = document.querySelector('.price-filter__slider');
+// Настройки для слайдера фильтра цены:
+if (document.querySelector('.price-filter')) {
+	const priceSlider = document.querySelector('.price-filter__slider');
 
-noUiSlider.create(priceSlider, {
-	start: [0, 200000],
-	connect: true,
-	tooltips: [wNumb({ decimals: 0 }), wNumb({ decimals: 0 })], //форматирование цен под ползунками в фильтре
-	range: {
-		'min': [0],
-		'max': [200000]
-	}
-});
+	noUiSlider.create(priceSlider, {
+		start: [0, 200000],
+		connect: true,
+		tooltips: [wNumb({ decimals: 0 }), wNumb({ decimals: 0 })], //форматирование цен под ползунками в фильтре
+		range: {
+			'min': [0],
+			'max': [200000]
+		}
+	});
 
-// При указании цены в полях для ввода, точки переместятся соответственно (4-1:17:25)
-const priceStart = document.getElementById('price-start');
-const priceEnd = document.getElementById('price-end');
-priceStart.addEventListener('change', setPriceValues);
-priceEnd.addEventListener('change', setPriceValues);
+	// При указании цены в полях для ввода, точки переместятся соответственно (4-1:17:25)
+	const priceStart = document.getElementById('price-start');
+	const priceEnd = document.getElementById('price-end');
+	priceStart.addEventListener('change', setPriceValues);
+	priceEnd.addEventListener('change', setPriceValues);
 
-function setPriceValues() {
-	let priceStartValue;
-	let priceEndValue;
-	if (priceStart.value != '') {
-		priceStartValue = priceStart.value;
+	function setPriceValues() {
+		let priceStartValue;
+		let priceEndValue;
+		if (priceStart.value != '') {
+			priceStartValue = priceStart.value;
+		}
+		if (priceEnd.value != '') {
+			priceEndValue = priceEnd.value;
+		}
+		priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
 	}
-	if (priceEnd.value != '') {
-		priceEndValue = priceEnd.value;
-	}
-	priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
 }
 
 //=================================================================================================================================
